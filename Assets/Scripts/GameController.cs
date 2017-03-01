2f23324dcb575b38212cb89d214e9f152cffa4c8
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2017-03-01 11:34:12 -0300

    Criando um script para Game Controller; gerando um asteróide aleatoriamente em x e fora da tela.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+
+	void Start()
+	{
+		SpawnWaves ();
+	}
+
+	//cria um asteróide em posição x aleatória e fora da tela
+	void SpawnWaves()
+	{
+		//define uma posição de acordo com o que foi passado
+		Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+		Quaternion spawnRotation = Quaternion.identity; //sem rotação
+		Instantiate (hazard, spawnPosition, spawnRotation); //instancia o tiro
+	}
+}
