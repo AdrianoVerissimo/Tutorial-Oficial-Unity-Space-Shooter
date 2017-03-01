491306416fae610e52802fbc5d54cbf315ec004f
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2017-03-01 11:58:41 -0300

    Add onda de asteróides aparecendo de tempo em tempo.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,18 +6,31 @@ public class GameController : MonoBehaviour {
 
 	public GameObject hazard;
 	public Vector3 spawnValues;
+	public int hazardCount = 1;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
 
 	void Start()
 	{
-		SpawnWaves ();
+		StartCoroutine(SpawnWaves ());
 	}
 
 	//cria um asteróide em posição x aleatória e fora da tela
-	void SpawnWaves()
+	IEnumerator SpawnWaves()
 	{
-		//define uma posição de acordo com o que foi passado
-		Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-		Quaternion spawnRotation = Quaternion.identity; //sem rotação
-		Instantiate (hazard, spawnPosition, spawnRotation); //instancia o tiro
+		yield return new WaitForSeconds (startWait);
+		while (true) {
+			for (int i = 0; i < hazardCount; i++) {
+				//define uma posição de acordo com o que foi passado
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity; //sem rotação
+				Instantiate (hazard, spawnPosition, spawnRotation); //instancia o tiro
+
+				//coroutine
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
 	}
 }
