24f73f56c886a9478aea6e3d99d043255a45f990
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2017-03-01 12:10:16 -0300

    Executando scripts em paralelo; criando script para destruir objeto depois de x tempo.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -13,23 +13,29 @@ public class GameController : MonoBehaviour {
 
 	void Start()
 	{
+		//executa uma função em paralelo, sem travar a execução até que ela seja terminada
 		StartCoroutine(SpawnWaves ());
 	}
 
 	//cria um asteróide em posição x aleatória e fora da tela
 	IEnumerator SpawnWaves()
 	{
-		yield return new WaitForSeconds (startWait);
+		yield return new WaitForSeconds (startWait); //espera X segundos para começar
+
+		//loop para ficar sempre executando
 		while (true) {
+			//para x asteróides
 			for (int i = 0; i < hazardCount; i++) {
 				//define uma posição de acordo com o que foi passado
 				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
 				Quaternion spawnRotation = Quaternion.identity; //sem rotação
 				Instantiate (hazard, spawnPosition, spawnRotation); //instancia o tiro
 
-				//coroutine
+				//espera x segundos para instanciar outro asteróide
 				yield return new WaitForSeconds (spawnWait);
 			}
+
+			//espera x segundos até começar outra onda de asteróides
 			yield return new WaitForSeconds (waveWait);
 		}
 	}
