9f088c59e4e508c1ca6f3bddc5246a35ae1ae0f9
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2017-03-01 11:15:13 -0300

    Add explosões ao asteróide encostar no laser e no jogador.

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
 
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
 	void OnTriggerEnter(Collider other)
 	{
 		//Debug.Log (other.name); //exibe no console nome do objeto que destruiu o asteróide
@@ -11,7 +14,13 @@ public class DestroyByContact : MonoBehaviour {
 		//se colidir com boundary, abortar script
 		if (other.CompareTag ("Boundary"))
 			return;
-		
+
+		Instantiate (explosion, transform.position, transform.rotation); //instancia explosão
+
+		//se quem colidiu foi o jogador
+		if (other.CompareTag ("Player"))
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation); //instancia explosão do jogador
+
 		//destrói o tiro e o asteróide
 		//desde que esteja no mesmo frame, não importa a ordem que o Destroy é utilizado: o objeto não é destruído imediatamente,
 		//e sim marcado para ser destruído no final do frame
