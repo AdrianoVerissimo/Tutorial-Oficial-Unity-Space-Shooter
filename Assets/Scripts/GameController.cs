5c9dd3eb6ad5e57d359529875dd4bb6a161b55b3
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2017-03-04 10:53:08 -0300

    Alternando o objeto do asteróide de forma aleatória.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
-	public GameObject hazard;
+	public GameObject[] hazards;
 	public Vector3 spawnValues;
 	public int hazardCount = 1;
 	public float spawnWait;
@@ -56,6 +56,7 @@ public class GameController : MonoBehaviour {
 		while (true) {
 			//para x asteróides
 			for (int i = 0; i < hazardCount; i++) {
+				GameObject hazard = hazards[Random.Range(0, hazards.Length)];
 				//define uma posição de acordo com o que foi passado
 				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
 				Quaternion spawnRotation = Quaternion.identity; //sem rotação
